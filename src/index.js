da7a158a9274876b87e55492a3f8b923b5158a31
huckjoo <huckjoo96@naver.com>
2022-04-03 21:24:31 +0900

    feat: implement redux in vanilla js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
+import { createStore } from 'redux';
+
 const plus = document.querySelector('#plus');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
-let count = 0;
-
-number.innerText = count;
-
-plus.addEventListener('click', handlePlus);
-minus.addEventListener('click', handleMinus);
-
-function handlePlus() {
-  count += 1;
-  updateText();
-}
-
-function handleMinus() {
-  count -= 1;
-  updateText();
-}
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  if (action.type === 'plus') {
+    return count + 1;
+  } else if (action.type === 'minus') {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
+
+number.innerText = 0;
+
+const countStore = createStore(countModifier);
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+plus.addEventListener('click', () => {
+  countStore.dispatch({ type: 'plus' });
+});
+minus.addEventListener('click', () => {
+  countStore.dispatch({ type: 'minus' });
+});
