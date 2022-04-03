e23258a35d3899c0f931930fac6e5cd982e344b1
huckjoo <huckjoo96@naver.com>
2022-04-03 23:34:50 +0900

    feat: implement redux in todo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,66 @@
+import { createStore } from 'redux';
+
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 
-const createToDo = (toDo) => {
-  const li = document.createElement('li');
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const addToDo = (text) => {
+  return { type: ADD_TODO, text };
 };
+const deleteToDo = (id) => {
+  return { type: DELETE_TODO, id };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE_TODO:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = ''; // 이전 ul을 모두 지워주고
+  toDos.forEach((toDo) => {
+    // 여기서 ul을 toDos에 맞게 모두 다시그려줌
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'DEL';
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn); // appendChild 순서바뀌면(33줄로가면) del이 안나타남
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos); // store값 변경되면 patinToDos함수실행
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  createToDo(toDo);
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener('submit', onSubmit);
